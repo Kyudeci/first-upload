7e3510ef728df8a99c9ff5b9f8e02f428142bb5e
Shakiem Johnson <sjohnson@nyc.yearup.org>
2018-10-30 15:17:06 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+for (var i = 99; i > 0; i--){
+  console.log(i + " crows on the wall. " + i + " crows." + " 1 fell down and became a wight. ") + console.log(i-1 + " crows on the wall.");
+  if (i == 2) {
+    console.log("1 crow on the wall. 1 single crow. It fell down and became a wight. There's no one left to defend Westeros now.");
+    break
+  }
+}
